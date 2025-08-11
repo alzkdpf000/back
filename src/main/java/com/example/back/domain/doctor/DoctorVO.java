e9d6fb794e2c28452f4176f249b5539ae6580845
mingzzang666 <mingzzang666@gmail.com>
2025-08-11 16:58:49 +0900

    doctor/info #3

---
+++ src/main/java/com/example/back/domain/doctor/DoctorVO.java
@@ -0,0 +1,9 @@
+package com.example.back.domain.doctor;
+
+
+public class DoctorVO extends MemberVO {
+    private String doctorLicenseNumber;
+    private String doctorSpecialty;
+    private Status doctorStatus;
+    private HospitalVO hospital;
+}
