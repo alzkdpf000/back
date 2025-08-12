dfd928e5f30e99a83690c057e46963d1057ef313
dev.temin018 <dev.gtaemin254@gmail.com>
2025-08-12 13:49:39 +0900

    회원가입페이지 #2

--- src/test/java/com/example/back/mapper/member/MemberMapperTests.java
+++ src/test/java/com/example/back/mapper/member/MemberMapperTests.java
@@ -1,12 +1,22 @@
 package com.example.back.mapper.member;
 
+import com.example.back.domain.member.MemberVO;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 @Slf4j
 public class MemberMapperTests {
+    @Autowired
+    MemberMapper memberMapper;
 
+    @Test
+    public void testInsertMember(){
+        MemberVO member = MemberVO.builder()
+                .memberEmail("test@gmail.com");
+
+    }
 
 }
