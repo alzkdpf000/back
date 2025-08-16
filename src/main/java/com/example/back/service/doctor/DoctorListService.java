941406f70948d403512faa0d79b45ace383cd246
mingzzang666 <mingzzang666@gmail.com>
2025-08-16 22:03:56 +0900

    doctor/list #3

--- src/main/java/com/example/back/service/doctor/DoctorListService.java
+++ src/main/java/com/example/back/service/doctor/DoctorListService.java
@@ -4,40 +4,12 @@ import com.example.back.domain.doctor.DoctorVO;
 import com.example.back.dto.doctor.DoctorListCriteriaDTO;
 import com.example.back.dto.doctor.DoctorListDTO;
 
-
-public interface DoctorListService {
-   //  목록
-   public DoctorListCriteriaDTO getList(int page);
-
-
-   default DoctorVO toDoctorVO(DoctorListDTO doctorListDTO){
-       return DoctorVO.builder()
-               .id(doctorListDTO.getId())
-               .doctorSpecialty(doctorListDTO.getDoctorSpecialty())
-               .doctorStatus(doctorListDTO.getDoctorStatus())
-               .id(doctorListDTO.getMemberId())
-               .hospitalId(doctorListDTO.getHospitalId())
-               .hospitalName(doctorListDTO.getHospitalName())
-               .hospitalPhone(doctorListDTO.getHospitalPhone())
-               .hospitalAddress(doctorListDTO.getHospitalAddress())
-               .createdDate(doctorListDTO.getCreatedDatetime())
-               .updatedDate(doctorListDTO.getUpdatedDatetime())
-               .build();
-   }
-}
-package com.example.back.service.doctor;
-
-import com.example.back.domain.doctor.DoctorVO;
-import com.example.back.dto.doctor.DoctorListCriteriaDTO;
-import com.example.back.dto.doctor.DoctorListDTO;
-
-
 public interface DoctorListService {
     //  목록
     public DoctorListCriteriaDTO getList(int page);
 
 
-    default DoctorVO toDoctorVO(DoctorListDTO doctorListDTO){
+    default DoctorVO gtoDoctorVO(DoctorListDTO doctorListDTO){
         return DoctorVO.builder()
                 .id(doctorListDTO.getId())
                 .doctorSpecialty(doctorListDTO.getDoctorSpecialty())
@@ -47,8 +19,8 @@ public interface DoctorListService {
                 .hospitalName(doctorListDTO.getHospitalName())
                 .hospitalPhone(doctorListDTO.getHospitalPhone())
                 .hospitalAddress(doctorListDTO.getHospitalAddress())
-                .createdDate(doctorListDTO.getCreatedDatetime())
-                .updatedDate(doctorListDTO.getUpdatedDatetime())
+                .createdDatetime(doctorListDTO.getCreatedDatetime())
+                .updatedDatetime(doctorListDTO.getUpdatedDatetime())
                 .build();
     }
 }
