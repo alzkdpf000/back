fd4c8681a07520cc8f19373e4029f93a35fcfbfb
mingzzang666 <mingzzang666@gmail.com>
2025-08-17 23:22:38 +0900

    doctor/list #3

--- src/main/java/com/example/back/controller/doctor/DoctorListController.java
+++ src/main/java/com/example/back/controller/doctor/DoctorListController.java
@@ -10,12 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/doctor/**")
+@RequestMapping("/doctor")
 public class DoctorListController {
     private final DoctorListService doctorListService;
 
     //    의사 목록
-    @GetMapping("list/{page}")
+    @GetMapping("/list/{page}")
     public String list(@PathVariable int page, Model model) {
         model.addAttribute("doctorsListCriteriaDTO", doctorListService.getList(page));
         return "doctor/doctor-list";
