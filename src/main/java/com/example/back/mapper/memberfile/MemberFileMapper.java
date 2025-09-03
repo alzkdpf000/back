600fe1cff284f0d82f72f4420edde59a0f058942
Ubuntu <ubuntu@ip-172-31-42-123.ap-northeast-2.compute.internal>
2025-09-03 20:58:37 +0900

    aws member file bug fix

--- src/main/java/com/example/back/mapper/memberfile/MemberFileMapper.java
+++ src/main/java/com/example/back/mapper/memberfile/MemberFileMapper.java
@@ -5,12 +5,15 @@ import com.example.back.dto.memberfile.MemberProfileDTO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.Optional;
+
 
 @Mapper
 public interface MemberFileMapper {
-
     //    회원 프로필 파일 가져오기
     public MemberProfileDTO getMemberProfile(Long memberId);
+
+    public Optional<MemberFileDTO> selectMemberProfile(Long memberId);
 
 //    파일 추가
     public void insertFile(MemberFileDTO MemberFileDTO);
@@ -29,9 +32,9 @@ public interface MemberFileMapper {
 //    회원파일 매핑 제거
     public void deleteMemberFile(Long memberId);
 //    파일 삭제
-    public void deleteFileByMemberId(Long memberId);
+    public void deleteFileByFileId(Long fileId);
 
-
+    public Optional<MemberFileDTO> selectMemberFile(Long memberId);
 
 
 
